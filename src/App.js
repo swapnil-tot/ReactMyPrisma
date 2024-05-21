1459be4f835326697eb99f2d5724ef4218af5d22
kuldip.panchal <swapnil.panchal@theonetechnologies.co.in>
2024-05-21 11:14:40 +0530

    [Update] : update the key

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to kp's world.</p>
+        <p>Welcome to kp's worlds.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
